a456459188369e1b1a948365ec01d9296c2cb063
Sebastian Schloesser <s.schloesser@gmail.com>
2024-11-18 15:30:24 -0500

    Actually run mqtt server

--- mqttServer.js
+++ mqttServer.js
@@ -1,5 +1,8 @@
+// mqttServer.js
+
 const admin = require('firebase-admin');
-const mqtt = require('mqtt');
+const aedes = require('aedes')();
+const net = require('net');
 if (process.env.NODE_ENV !== 'prod') {
     require('dotenv').config();
 }
@@ -13,13 +16,20 @@ admin.initializeApp({
   databaseURL: 'https://spatialmedia-22177-default-rtdb.firebaseio.com/'
 });
 
-// Connect to MQTT broker
-const client = mqtt.connect('mqtt://your-mqtt-broker-url');
+// Create and start the MQTT broker
+const brokerPort = 1883;
+const server = net.createServer(aedes.handle);
 
+server.listen(brokerPort, function () {
+  console.log('MQTT broker started and listening on port', brokerPort);
+});
+
+// Firebase database reference
 const db = admin.database();
 const ref = db.ref('/beacons');
 
+// Publish Firebase updates to the MQTT broker
 ref.on('value', (snapshot) => {
   const data = snapshot.val();
-  client.publish('beacon_users', JSON.stringify(data));
+  aedes.publish({ topic: 'beacon_users', payload: JSON.stringify(data) });
 });
