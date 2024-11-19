43f41b06e0f93985392770b06009541dec3031ae
Sebastian Schloesser <s.schloesser@gmail.com>
2024-11-19 17:53:37 -0500

    Switch back to forwarding to an existing mqtt broker

--- mqttServer.js
+++ mqttServer.js
@@ -1,46 +1,23 @@
 // mqttServer.js
 
 const admin = require('firebase-admin');
-const aedes = require('aedes')();
-const http = require('http');
-const ws = require('websocket-stream');
+const mqtt = require('mqtt');
 if (process.env.NODE_ENV !== 'prod') {
     require('dotenv').config();
 }
 
-// Parse the JSON string from the environment variable
-const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
-
 // Initialize Firebase Admin SDK
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://spatialmedia-22177-default-rtdb.firebaseio.com/'
 });
 
-// Create and start the MQTT broker over WebSockets
-const brokerPort = process.env.PORT || 3000; // Use Heroku's assigned port
-const server = http.createServer();
-
-// Parse the VALID_USERS environment variable
-const validUsers = JSON.parse(process.env.VALID_USERS || '{}');
-
-// Add authentication to the MQTT broker
-aedes.authenticate = (client, username, password, callback) => {
-  const isValidUser = validUsers[username] && validUsers[username] === password.toString();
-  if (isValidUser) {
-    callback(null, true);
-  } else {
-    const error = new Error('Authentication Failed');
-    error.returnCode = 4; // MQTT 3.1.0: Bad username or password
-    callback(error, false);
-  }
-};
-
-// Connect broker to websockets
-ws.createServer({ server }, aedes.handle);
-
-server.listen(brokerPort, function () {
-  console.log('MQTT broker over WebSockets started and listening on port', brokerPort);
+// Connect to the existing MQTT broker with authentication
+const mqttBrokerUrl = process.env.MQTT_BROKER_URL;
+const mqttClient = mqtt.connect(mqttBrokerUrl, {
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD
 });
 
 // Firebase database reference
@@ -50,7 +27,20 @@ const ref = db.ref('/beacons');
 // Publish Firebase updates to the MQTT broker
 ref.on('value', (snapshot) => {
   const data = snapshot.val();
-  aedes.publish({ topic: 'beacon_users', payload: JSON.stringify(data) });
+  mqttClient.publish('sebschlo/feeds/beacon-users', JSON.stringify(data), (err) => {
+    if (err) {
+      console.error('Failed to publish message:', err);
+    } else {
+      console.log('Message published to topic beacon_users');
+    }
+  });
 });
 
+// Handle connection events
+mqttClient.on('connect', () => {
+  console.log('Connected to MQTT broker');
+});
 
+mqttClient.on('error', (err) => {
+  console.error('MQTT connection error:', err);
+});
